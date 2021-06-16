2f60047c8ae1c87f488ce482caa272349d7f948f
U1-nix <er.cepurits@gmail.com>
2021-06-16 12:31:27 +0300

    MinimisationTest: Improved assertion statements

--- src/test/java/MinimisationTest.java
+++ src/test/java/MinimisationTest.java
@@ -8,6 +8,7 @@ import testingFunctions.RosenbrockValley;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MinimisationTest {
@@ -26,6 +27,7 @@ public class MinimisationTest {
         arguments.add(4.9);
         arguments.add(-5.1);
 
+        // De Jong function
         Minimizable deJong = DeJong.getInstance();
         List<Double> minimisedArguments = NelderMead.minimise(arguments, deJong);
 
@@ -33,48 +35,42 @@ public class MinimisationTest {
         for (double argument : minimisedArguments) {
             condition = (argument < 0.5) && (argument > -0.5);
         }
-
         // De Jong minimised argument test (precision +/- 0.5)
         assertTrue(condition);
 
         double minimisedFunctionValue = deJong.calculate(minimisedArguments);
-        condition = (minimisedFunctionValue < 0.5) && (minimisedFunctionValue > -0.5);
-
         // De Jong minimised function value test (precision +/- 0.5)
-        assertTrue(condition);
+        assertEquals(0.0, minimisedFunctionValue, 0.5);
 
+
+        // Axis parallel hyper-ellipsoid function
         Minimizable axisParallelHyperEllipsoidFunction = AxisParallelHyperEllipsoidFunction.getInstance();
         minimisedArguments = NelderMead.minimise(arguments, axisParallelHyperEllipsoidFunction);
 
         for (double argument : minimisedArguments) {
             condition = (argument < 0.5) && (argument > -0.5);
         }
-
         // Axis parallel hyper-ellipsoid function minimised argument test (precision +/- 0.5)
         assertTrue(condition);
 
         minimisedFunctionValue = axisParallelHyperEllipsoidFunction.calculate(minimisedArguments);
-        condition = (minimisedFunctionValue < 0.5) && (minimisedFunctionValue > -0.5);
-
         // Axis parallel hyper-ellipsoid function minimised argument test (precision +/- 0.5)
-        assertTrue(condition);
+        assertEquals(0.0, minimisedFunctionValue, 0.5);
+
 
         arguments.clear();
         arguments.add(0.7);
         arguments.add(-0.9);
         arguments.add(-0.5);
         arguments.add(1.3);
-        //arguments.add(0.5);
 
-
+        // Rosenbrock's valley function
         Minimizable rosenbrockValley = RosenbrockValley.getInstance();
 
         minimisedArguments = NelderMead.minimise(arguments, rosenbrockValley);
 
         minimisedFunctionValue = rosenbrockValley.calculate(minimisedArguments);
-        condition = (minimisedFunctionValue < 3.0) && (minimisedFunctionValue > -3.0);
-
         // Rosenbrock's valley minimised function value test (precision +/- 3.0)
-        assertTrue(condition);
+        assertEquals(0.0, minimisedFunctionValue, 3.0);
     }
 }
